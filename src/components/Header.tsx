460e2ee48c478d5f32d013c9d169a90471502afb
Minja Ćućula <cuculaminja@proton.me>
2025-03-27 18:33:23 +0100

    Header - adding styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,28 +4,31 @@ function Header() {
   return (
     <header className="header">
       <div className="header__logo-box">
-        <img src={logoIcon} alt="Logo Icon" className="logo-box__icon" />
-        <p className="logo-box__text">Polovni automobili</p>
+        <img src={logoIcon} alt="Logo Icon" className="header__icon" />
+        <p className="header__heading">
+          <span className="header__heading--first">Polovni</span>
+          <span className="header__heading--second">Automobili</span>
+        </p>
       </div>
       <nav className="header__nav">
-        <ul className="nav__list">
-          <li className="nav__item">
-            <a href="#" className="nav__link">
+        <ul className="header__nav-list">
+          <li className="header__nav-item">
+            <a href="#" className="header__nav-link">
               Početna
             </a>
           </li>
-          <li className="nav__item">
-            <a href="#" className="nav__link">
-              O nama
+          <li className="header__nav-item">
+            <a href="#" className="header__nav-link">
+              Kontakt
             </a>
           </li>
-          <li className="nav__item">
-            <a href="#" className="nav__link">
+          <li className="header__nav-item">
+            <a href="#" className="header__nav-link">
               Uloguj se
             </a>
           </li>
-          <li className="nav__item">
-            <a href="#" className="nav__link">
+          <li className="header__nav-item">
+            <a href="#" className="header__nav-link">
               Registruj se
             </a>
           </li>
